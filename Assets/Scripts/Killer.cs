b3c6ff06c68235c92b3733ea119020e14758d83f
kleyson klaus <kleyson.zur44@gmail.com>
2022-09-26 23:31:53 -0500

    feat(killer): when player touch it the game will be reset

---
+++ Assets/Scripts/Killer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // no se usara
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // no se usara
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Game.obj.gameOver();
+        }
+    }
+}
